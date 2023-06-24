9072ea3d0a7df7b5d6b15f87874e1b071410cba2
thoratica <me@tica.fun>
2023-06-25 01:49:05 +0900

    :sparkles: Added getSticker route

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import stickers from '~/stickers/stickers.json' assert { type: 'json' };
 import { graphql } from '~/lib/graphql.ts';
 
 stickers.sort((a, b) => a.date - b.date);
-const { procedure, router } = initTRPC.create();
+const { middleware, procedure, router } = initTRPC.create();
 
 export const appRouter = router({
   getStickers: procedure
@@ -57,6 +57,50 @@ export const appRouter = router({
         });
 
       return pageStickers;
+    }),
+  getSticker: procedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input: { id } }) => {
+      const sticker = stickers.find((sticker) => sticker.id === id);
+      if (!sticker) throw new Error('Sticker not found');
+
+      const authors = [...new Set(sticker.authors)];
+      const query = `query (${authors
+        .map((author) => {
+          return `$id${author}: String`;
+        })
+        .join(', ')}) {
+  ${authors
+    .map((author) => {
+      return `id${author}: userstatus(id: $id${author}) { nickname }`;
+    })
+    .join('\n  ')}
+}`;
+
+      const data = await graphql<{ [key: string]: { nickname: string } }>(
+        query,
+        Object.fromEntries(
+          authors.map((author) => {
+            return [`id${author}`, author];
+          }),
+        ),
+      );
+      const nicknames = Object.entries(data).reduce((prev, curr) => {
+        prev[curr[0].slice(2)] = curr[1].nickname;
+        return prev;
+      }, {} as { [key: string]: string });
+
+      const pageSticker: Omit<typeof stickers[number], 'authors'> & {
+        authors: { id: string; name: string }[];
+      } = {
+        ...sticker,
+        authors: sticker.authors.map((author) => ({
+          id: author,
+          name: nicknames[author],
+        })),
+      };
+
+      return pageSticker;
     }),
 });
 
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,15 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { type AppRouter, appRouter } from '~/router.ts';
 import { getCSRFToken } from '~/lib/graphql.ts';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'https://daisy.lol',
+];
+
 function handler(req: Request) {
-  if (req.method === 'HEAD') return new Response();
+  if (['HEAD', 'OPTIONS'].includes(req.method))
+    return Promise.resolve(new Response());
 
   return fetchRequestHandler<AppRouter>({
     endpoint: '/trpc',
@@ -20,4 +27,17 @@ const port = parseInt(Deno.env.get('PORT') ?? '4000');
 
 await getCSRFToken();
 
-serve(handler, { port });
+serve(
+  (req) =>
+    handler(req).then((res: Response) => {
+      if (allowedOrigins.includes(req.headers.get('Origin')!)) {
+        res.headers.append(
+          'Access-Control-Allow-Origin',
+          req.headers.get('Origin')!,
+        );
+        res.headers.append('Access-Control-Allow-Headers', '*');
+      }
+      return res;
+    }),
+  { port },
+);
